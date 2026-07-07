77a1fa1812c3d06430baf651654d8423598702fc
Simran Keshri <simrangrd373@gmail.com>
2026-07-07 13:51:59 +0530

    Time: 1 ms (99.93%), Space: 46.7 MB (63.37%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,25 +8,80 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ //heap and priority Queue
+ 
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+//         for (ListNode node : lists) {
+//             if (node != null)
+//                 pq.offer(node);
+//         }
+//         ListNode dummy = new ListNode(-1);
+//         ListNode tail = dummy;
+
+//         while (!pq.isEmpty()) {
+//             ListNode curr = pq.poll();
+
+//             tail.next = curr;
+//             tail = tail.next;
+
+//             if (curr.next != null)
+//                 pq.offer(curr.next);
+//         }
+//         return dummy.next;
+//     }
+// }
+
+
+//divide and conquer techniques
 class Solution {
+
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
-        for (ListNode node : lists) {
-            if (node != null)
-                pq.offer(node);
+
+        if (lists == null || lists.length == 0)
+            return null;
+
+        int interval = 1;
+
+        while (interval < lists.length) {
+
+            for (int i = 0; i + interval < lists.length; i += interval * 2) {
+
+                lists[i] = merge(lists[i], lists[i + interval]);
+            }
+
+            interval *= 2;
         }
+
+        return lists[0];
+    }
+
+    private ListNode merge(ListNode a, ListNode b) {
+
         ListNode dummy = new ListNode(-1);
         ListNode tail = dummy;
 
-        while (!pq.isEmpty()) {
-            ListNode curr = pq.poll();
+        while (a != null && b != null) {
 
-            tail.next = curr;
+            if (a.val <= b.val) {
+                tail.next = a;
+                a = a.next;
+            } else {
+                tail.next = b;
+                b = b.next;
+            }
+
             tail = tail.next;
-
-            if (curr.next != null)
-                pq.offer(curr.next);
         }
+
+        if (a != null)
+            tail.next = a;
+
+        if (b != null)
+            tail.next = b;
+
         return dummy.next;
     }
 }
